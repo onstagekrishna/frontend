f378ee768c73610c76b6592f10316a0f36cae5f4
onstagekrishna <ordersmmdc@gmail.com>
2026-09-25 14:43:56 +0530

    mobile updated

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -303,7 +303,7 @@ function Header() {
 
         <span>
           <IoCallOutline />
-          <a href="tel:+918447752663">+91 9717404450</a>
+          <a href="tel:+919717404450">+91 9717404450</a>
         </span>
       </div>
     </div>
